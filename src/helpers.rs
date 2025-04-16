d623c63d3009f79212bdf0a59df89ce2747f2adb
Cole Sashkin <40610719+teleplayer123@users.noreply.github.com>
2025-04-16 14:01:32 -0700

    Update helpers.rs

--- src/helpers.rs
+++ src/helpers.rs
@@ -9,3 +9,24 @@ fn hex_to_bytes(s: &str) -> Option<Vec<u8>> {
         None
     }
 }
+
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    {
+        let mut file = File::create("test")?;
+        // Write a slice of bytes to the file
+        file.write_all(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])?;
+    }
+
+    {
+        let mut file = File::open("test")?;
+        // read the same file back into a Vec of bytes
+        let mut buffer = Vec::<u8>::new();
+        file.read_to_end(&mut buffer)?;
+        println!("{:?}", buffer);
+    }
+
+    Ok(())
+}
